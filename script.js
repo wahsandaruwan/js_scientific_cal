f9fddbcb0f3321167750acf247c87ff0d1e3c679
wahsandaruwan <wahsandaruwan@gmail.com>
2021-01-28 16:26:32 +0530

    js common variables

---
+++ script.js
@@ -0,0 +1,7 @@
+// Common Variables
+var numberButtonsNormal = document.querySelectorAll('.normal .row .num');
+var operatorButtonsNormal = document.querySelectorAll('.normal .row .op');
+
+for(var x of operatorButtonsNormal){
+    console.log(x);
+}
