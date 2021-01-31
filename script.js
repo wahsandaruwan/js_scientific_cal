5c615bff48ff6bde7beec4d3d6fc450eb808f874
wahsandaruwan <wahsandaruwan@gmail.com>
2021-02-01 01:05:44 +0530

    Added major 4 operators

--- script.js
+++ script.js
@@ -1,53 +1,87 @@
 // ---Common variables & nodeLists---
 var result = '';
-var expression = '';
+var input = '';
 var operation = undefined;
 
 var numberButtonsNormal = document.querySelectorAll('.normal .row .num');
 var operatorButtonsNormal = document.querySelectorAll('.normal .row .op');
 var clrButtonsNormal = document.querySelectorAll('.normal .row .clr');
+var equalButtonNormal = document.querySelector('.normal .row .equal');
 var resultDisplay = document.querySelector('.display .dis1');
-var expressionDisplay = document.querySelector('.display .dis2');
+var inputDisplay = document.querySelector('.display .dis2');
 
 // ---Functions---
 
 // Clear function
 function clearAll(){
     result = '';
-    expression = '';
+    input = '';
     operation = undefined;
 }
 
 // Delete function
 function deleteCurrent(){
-    expression = expression.slice(0, -1);
+    input = input.slice(0, -1);
 }
 
 // Append function
 function appendNumber(number){
-    if(number === '.' && expression.includes('.')){
-        return;
+    // Period handling
+    if(number === '.'){
+        if(input.includes('.')) return;
+        if(input.length == 0) input = input + 0;
     }
-    expression = expression + number;
-    console.log(expression);
+    input = input + number;
 }
 
 // Operator identification function
-function identifyOperator(){
+function identifyOperator(operator){
+    if(input === '') return;
+    if(result !== '') calculate();
 
+    operation = operator;
+    result = input;
+    input = '';
 }
 
 // Compute function
-function compute(){
+function calculate(){
+    var calculation;
+    var res = parseFloat(result);
+    var inp = parseFloat(input);
 
+    if(isNaN(res) || isNaN(inp)) return;
+    switch(operation){
+        case '+':
+            calculation = res + inp;
+            break;
+        case '-':
+            calculation = res - inp;
+            break;
+        case '*':
+            calculation = res * inp;
+            break;
+        case '/':
+            calculation = res / inp;
+            break;
+        default:
+            return;
+    }
+
+    input = calculation;
+    operation = undefined;
+    result = '';
 }
 
 // Display update function
 function updateDisplay(){
-    expressionDisplay.innerText = expression;
+    inputDisplay.innerText = input;
+    resultDisplay.innerText = result;
 }
 
-// ---Get numbers process---
+// ---Event listeners for elements---
+
+// Get numbers
 // The forEach() method calls the provided function once for each element of the array. The provided function may perform any kind of operation on the elements of the given array.
 numberButtonsNormal.forEach(function(number){
     number.addEventListener('click', function(){
@@ -56,10 +90,25 @@ numberButtonsNormal.forEach(function(number){
     });
 });
 
-// ---Clear & Delete process---
+// Clear & Delete
 clrButtonsNormal.forEach(function(clr){
     clr.addEventListener('click', function(){
-        clr.innerText === 'AC' ? clearAll() : deleteCurrent();
+        if(clr.innerText === 'AC') clearAll();
+        else deleteCurrent();
         updateDisplay();
     });
+});
+
+// Get operators
+operatorButtonsNormal.forEach(function(op){
+    op.addEventListener('click', function(){
+        identifyOperator(op.innerText);
+        updateDisplay();
+    });
+});
+
+// Calculate
+equalButtonNormal.addEventListener('click', function(){
+    calculate();
+    updateDisplay();
 });
